a41530479bca3c91da3206b98b9365932071ee26
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-03-13 20:13:54 +0530

    adding a firebase authetication

--- foodi-client/src/firebase/firebase.config.js
+++ foodi-client/src/firebase/firebase.config.js
@@ -25,12 +25,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBLO95ZL0AUlW7uhehngFVVFcqE7FeQlk8",
-  authDomain: "fir-foodi-client-8e286.firebaseapp.com",
-  projectId: "fir-foodi-client-8e286",
-  storageBucket:"fir-foodi-client-8e286.appspot.com",
-  messagingSenderId:"815546400330",
-  appId:"1:815546400330:web:08ba22fc8ae3c76a28f0b3"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
