bf4a088fb1ae28a2f3e30a5621fb8efc95e4d543
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-03-13 22:23:48 +0530

    add get all payment routes for admin

--- foodi-server1/api/routes/paymentRoutes.js
+++ foodi-server1/api/routes/paymentRoutes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/verifyToken');
 const paymentController = require('../controllers/paymentController');
+const Payment = require('../models/payments');
 
 // Linking the route paths to their respective controller functions
-router.post('/', verifyToken, paymentController.processPayment);
-router.get('/', verifyToken, paymentController.getPayments);
+router.post('/',  paymentController.processPayment);
+router.get('/',  paymentController.getPayments);
+// get all payments by user
+
+router.get('/all',  paymentController.getAllPayments);
+
+
 
 module.exports = router;
