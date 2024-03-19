a7965dc113b65cb476326d29f96ca3676d3765e2
SHIVAM GUPTA <118923085+ShivamGupta987@users.noreply.github.com>
2024-03-19 20:19:47 +0530

    Update useAxiosPublic.jsx

--- foodi-client/src/hooks/useAxiosPublic.jsx
+++ foodi-client/src/hooks/useAxiosPublic.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 const axiosPublic =  axios.create({
-    baseURL: 'https://complete-mern-project-13.onrender.com/',
+    baseURL: 'https://complete-mern-project-13.onrender.com',
   })
 
 const useAxiosPublic = () => {
